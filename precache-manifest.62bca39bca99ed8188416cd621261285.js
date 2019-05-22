dfbad9243ccdf1c20eb0aeadca79e2036728b5ef
Hannes <hannes@rang.de>
2019-05-22 22:37:43 +0200

    Updates

---
+++ precache-manifest.62bca39bca99ed8188416cd621261285.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b0f85159bb204e213c05",
+    "url": "./static/js/main.b0f85159.chunk.js"
+  },
+  {
+    "revision": "3237f2e0a4737d07ece7",
+    "url": "./static/js/1.3237f2e0.chunk.js"
+  },
+  {
+    "revision": "b0f85159bb204e213c05",
+    "url": "./static/css/main.fe3f9b82.chunk.css"
+  },
+  {
+    "revision": "d45249c470c32baa4ebe182d8bd9a560",
+    "url": "./index.html"
+  }
+];
